1e9a5ac1aa148fb98cd6f058afa2a8eb784d2d25
Daiara Dyba <daiaradyba@gmail.com>
2024-03-29 18:44:56 -0300

    HTTP Firebase SETUP PiscaLed

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -8,61 +8,147 @@
 #include "esp_http_client.h"
 #include "esp_netif.h"
 #include "driver/gpio.h"
+#include "freertos/queue.h"
 
 #define WIFI_SSID      "Dyba_Bernardelli_2G"
 #define WIFI_PASS      "123581321"
+#define MAX_HTTP_RECV_BUFFER 512
 
 static const char *TAG = "wifi_http_example";
 
-
+QueueHandle_t queue_tempo;
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
-    switch(evt->event_id) {
+
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+
+      switch(evt->event_id) {
         case HTTP_EVENT_ERROR:
-            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
             break;
         case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
             break;
         case HTTP_EVENT_HEADER_SENT:
-            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
             break;
         case HTTP_EVENT_ON_HEADER:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER");
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
+            // Check for chunked encoding is set to false
             if (!esp_http_client_is_chunked_response(evt->client)) {
-                printf("%.*s\n", evt->data_len, (char*)evt->data);
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
             }
             break;
         case HTTP_EVENT_ON_FINISH:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is not chunked
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
             break;
+    case HTTP_EVENT_REDIRECT:
+    
+    ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
+    break;
         case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
-            break;
-        case HTTP_EVENT_REDIRECT: // Adicionado tratamento para HTTP_EVENT_REDIRECT
-            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
-            break;
-        default:
-            ESP_LOGD(TAG, "Unhandled event (%d)", evt->event_id);
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            if (output_buffer != NULL) {
+                // Free memory of output buffer
+                free(output_buffer);
+                output_buffer = NULL;
+                output_len = 0;
+            }
             break;
     }
     return ESP_OK;
 }
-// Função para realizar a requisição HTTP GET
-void http_get_task(void *pvParameters) {
+
+void http_get_task(void *pvParameters)
+{
+
+    esp_rom_gpio_pad_select_gpio (GPIO_NUM_25);
+
+    //Define como saída
+     gpio_set_direction (GPIO_NUM_25, GPIO_MODE_INPUT);
+
+    //PullUp para acionar quando lvl = gnd
+     gpio_set_pull_mode(GPIO_NUM_25, GPIO_PULLUP_ONLY);
+
+
+    char output_buffer[MAX_HTTP_RECV_BUFFER] = {0};  // Buffer to store response of http request
     esp_http_client_config_t config = {
-        .url = "http://httpbin.org/get",
+        .url = "https://carrinhoeducativo-default-rtdb.firebaseio.com/code.json", // Substitua pela sua URL do Firebase
         .event_handler = _http_event_handler,
+        .skip_cert_common_name_check = true, //ignorar certificado por enquanto
+        .user_data = output_buffer  // Pass a pointer to a buffer to store the response
     };
     esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_http_client_perform(client);
 
-    // Limpeza
-    esp_http_client_cleanup(client);
-    vTaskDelete(NULL);
-}
+    while(1){
+        while (gpio_get_level(GPIO_NUM_25) == 1) {
+            vTaskDelay(pdMS_TO_TICKS(100)); // Evita checagem contínua, diminuindo o uso da CPU
+        }
+        // Debounce simples
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+        
+    if (gpio_get_level(GPIO_NUM_25) == 0) {
+        esp_http_client_cleanup(client);
+        // necessario pq se nao depois de um tempo nao consigo fazer novas requisições
+        client = esp_http_client_init(&config);
+
+            ESP_LOGI(TAG, "Botão pressionado, fazendo solicitação HTTP...");
+            // Realiza a solicitação HTTP
+            esp_err_t err = esp_http_client_perform(client);
+        if (err == ESP_OK) {
+        ESP_LOGI(TAG, "Got data: %s", output_buffer);
+        // Parse do JSON para extrair o valor do tempo
+        int tempo_piscada;
+        sscanf(output_buffer, "\"%d\"", &tempo_piscada);
+        printf("Tempo Piscada get task %d\n",tempo_piscada);
+
+        // Limpa o buffer para a próxima leitura
+        memset(output_buffer, 0, MAX_HTTP_RECV_BUFFER);
+
+        // Enviar o valor do tempo para a fila
+
+        if (xQueueSend(queue_tempo, &tempo_piscada, portMAX_DELAY) != pdPASS) {
+            ESP_LOGE(TAG, "Failed to send to the queue");
+        }
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+
+           
+        }
+
+        // Aguarda um momento antes de permitir outra checagem para evitar múltiplas detecções
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+
+    }
+
+
 
 // Handler para eventos do Wi-Fi
 void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
@@ -119,6 +205,7 @@ void wifi_init() {
 
 void vtask_blink_led(void *pvParameter){
     //Especifica que vai ser um GPIO 
+
      esp_rom_gpio_pad_select_gpio (GPIO_NUM_32);
      esp_rom_gpio_pad_select_gpio (GPIO_NUM_33);
 
@@ -128,17 +215,23 @@ void vtask_blink_led(void *pvParameter){
 
     printf ("GPIO Configurado\n");
     int uiCounter = 0;
+    int tempo_piscada = 1; // Tempo padrão de piscada em segundos
 
     for(;;){
+        if (xQueueReceive(queue_tempo, &tempo_piscada, 0) == pdPASS) {
+            ESP_LOGI(TAG, "Tempo de piscada atualizado para %d segundos", tempo_piscada);
+        }
         gpio_set_level(GPIO_NUM_32, (uiCounter%2));
         gpio_set_level(GPIO_NUM_33, ((uiCounter+1)%2));
-        vTaskDelay(pdMS_TO_TICKS(10000));
+        vTaskDelay(pdMS_TO_TICKS(tempo_piscada * 1000));
         uiCounter++;
         printf ("Pisquei\n");
     }
 
 } 
 void app_main(void) {
+    // criar fila 
+     queue_tempo = xQueueCreate(10, sizeof(int));
     // Inicializa o Wi-Fi
      xTaskCreate(vtask_blink_led     ,
                 "vtask_blink_led"   ,
