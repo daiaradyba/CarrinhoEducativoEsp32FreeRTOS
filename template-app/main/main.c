4f8117181e8e18957a787b30d88ea33af921fdbf
Daiara Dyba <daiaradyba@gmail.com>
2024-05-27 15:10:35 -0300

    Buzzer melody and notes

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -56,7 +56,7 @@ gpio_num_t gpio_to_sobra = GPIO_NUM_26; //PINO D26
 
 gpio_num_t gpio_01 = GPIO_NUM_12; //PINO D12 - wifi
 gpio_num_t gpio_02 = GPIO_NUM_13; //PINO D13 - LED COM PWM
-gpio_num_t gpio_03 = GPIO_NUM_14; //PINO D14
+gpio_num_t gpio_03 = GPIO_NUM_14; //PINO D14 - Buzzer
 gpio_num_t gpio_04 = GPIO_NUM_23; //PINO D23
 gpio_num_t gpio_05 = GPIO_NUM_22; //PINO D22
 gpio_num_t gpio_06 = GPIO_NUM_21; //PINO D21
@@ -69,6 +69,107 @@ gpio_num_t gpio_12 = GPIO_NUM_27; //PINO D27
 
 #define LEDC_TIMER_10_BIT  10
 #define LEDC_BASE_FREQ     5000
+#define buzzer_pin gpio_03
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST 0
+
+int duracao_nota = 1000; // 1 segundo default;
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+
 
 void setup_pwm_timer() {
     ledc_timer_config_t ledc_timer = {
@@ -344,7 +445,143 @@ void config_saidas(){
 }
 
 
+
+
+void playTone(int frequency, int duration) {
+    if (frequency == REST) {
+        vTaskDelay(duration / portTICK_PERIOD_MS);
+    } else {
+        ledc_timer_config_t timer_conf = {
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .duty_resolution = LEDC_TIMER_10_BIT,
+            .timer_num = LEDC_TIMER_1,
+            .freq_hz = frequency
+        };
+        ledc_timer_config(&timer_conf);
+
+        ledc_channel_config_t channel_conf = {
+            .gpio_num = buzzer_pin,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .channel = LEDC_CHANNEL_5,
+            .intr_type = LEDC_INTR_FADE_END,
+            .timer_sel = LEDC_TIMER_1,
+            .duty = 512,
+            .hpoint = 0
+        };
+        ledc_channel_config(&channel_conf);
+
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, 512);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5);
+
+        vTaskDelay(duration / portTICK_PERIOD_MS);
+
+        ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, 0);
+    }
+}
+
+void playMelody_tetris(void *pvParameters) {
     
+        // change this to make the song slower or faster
+        int tempo=144; 
+
+        int melody[] = {
+
+    //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+    
+    NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+    NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+    NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+    NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+    NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+    NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+    NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+    NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+
+
+    };
+
+    // sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+    // there are two values per note (pitch and duration), so for each note there are four bytes
+    int notes=sizeof(melody)/sizeof(melody[0])/2; 
+
+    // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+    int wholenote = (60000 * 4) / tempo;
+
+    int divider = 0, noteDuration = 0;
+    
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+        divider = melody[thisNote + 1];
+        if (divider > 0) {
+            noteDuration = wholenote / divider;
+        } else if (divider < 0) {
+            noteDuration = (wholenote / abs(divider)) * 1.5;
+        }
+
+        playTone(melody[thisNote], noteDuration * 0.9);
+        vTaskDelay(noteDuration / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL); // Delete the task when done
+}
+void playMelody_mario(void *pvParameters) {
+          // change this to make the song slower or faster
+        int tempo=300; 
+        int melody[] = {
+
+    //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+    NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,8, //1
+    NOTE_G5,4, REST,4, NOTE_G4,8, REST,4, 
+    NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 3
+    NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+    NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+    REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+    NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 3
+    NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+    NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+    REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+
+
+    };
+
+    // sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+    // there are two values per note (pitch and duration), so for each note there are four bytes
+    int notes=sizeof(melody)/sizeof(melody[0])/2; 
+
+    // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+    int wholenote = (60000 * 4) / tempo;
+
+    int divider = 0, noteDuration = 0;
+    
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+        divider = melody[thisNote + 1];
+        if (divider > 0) {
+            noteDuration = wholenote / divider;
+        } else if (divider < 0) {
+            noteDuration = (wholenote / abs(divider)) * 1.5;
+        }
+
+        playTone(melody[thisNote], noteDuration * 0.9);
+        ESP_LOGI("melody", "duration %d",noteDuration);
+        vTaskDelay(noteDuration / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL); // Delete the task when done
+}
+
+
+void buzzer_melody(int value){
+    ESP_LOGI("Melody","entrei buzzer melody com value %d",value );
+    if (value == 1){
+         xTaskCreate(playMelody_tetris, "playMelody_tetris", 2048, NULL, 5, NULL);
+    }
+    if (value == 2){
+         xTaskCreate(playMelody_mario, "playMelody_mario", 2048, NULL, 5, NULL);
+    }
+    else{
+        playTone(value, duracao_nota);
+    }
+
+}
+
 
 void activate_pwm(int choice, int pwm) {
     ledc_channel_t channel;
@@ -1038,6 +1275,14 @@ int execute_command(const char* command, int value) {
     else if (strcmp(command, "ler") == 0 || strcmp(command, "\"ler") == 0) {
         int voltage = ler_adc(value);
          ESP_LOGI(TAG, "Valor ler ADC: %d", value);
+ 
+    }
+    else if (strcmp(command, "dur") == 0 || strcmp(command, "\"dur") == 0) { // define duração nota
+    duracao_nota = value;
+ 
+    }
+    else if (strcmp(command, "buzzerm") == 0 || strcmp(command, "\"buzzerm") == 0) {
+    buzzer_melody(value);
  
     }
     else if (strcmp(command, "pwmled") == 0 || strcmp(command, "\"pwmled") == 0) {
